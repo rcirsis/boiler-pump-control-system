acd660a82d951ddb26a00f31ff13d0ad02d78942
Romualds Cirsis <r.cirsis@gmail.com>
2022-03-02 16:59:47 +0200

    Project Draft (Init)
    Tested on DevPCB and it is working. A lot of fixes should be done.

---
+++ credentials.h
@@ -0,0 +1,15 @@
+/* 
+	Example of hidden data
+	by it selfe is created to exlude it from Git
+*/
+
+// AP the device will be trying to connect, multiple pionts can be used.
+char *wifi_data[][2] = {
+	{"Point A", "Point A Password"}, 
+	{"Point B", "Point B Password"},
+	{"Point C", "Point C Password"}
+};
+
+// Link where data will be posted
+char* server = "http://www.google.com/php/data.php";
+
